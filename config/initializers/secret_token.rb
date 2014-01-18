c58396d0c5cfb97f7e09ac82c6c5166d670f4875
davidhurley <dhurls99@gmail.com>
2014-01-18 15:27:42 -0500

    what what

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Civso2::Application.config.secret_key_base = '195be3bfd9682f17e656b6574ce531ff07997c1dfe89ba2f2c84b79b8dd119b984c6fdb94cfdcfaf25473b9d3107a381631b1f1793e457f8c994590edf96af30'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Civso2::Application.config.session_store :cookie_store, key: '_civso2_session'
