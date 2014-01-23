dba53d0a09481a774138f4553f2c1120d2557ad7
davidhurley <dhurls99@gmail.com>
2014-01-23 18:43:06 -0500

    add user names

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140123193317) do
+ActiveRecord::Schema.define(version: 20140123234149) do
 
   create_table "projects", force: true do |t|
     t.string   "headline"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20140123193317) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
