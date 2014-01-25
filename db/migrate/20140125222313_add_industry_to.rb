7805fcb9d806decc3c58bda83fec6aab85bf8b9c
davidhurley <dhurls99@gmail.com>
2014-01-25 17:26:49 -0500

    add industry

---
+++ db/migrate/20140125222313_add_industry_to.rb
@@ -0,0 +1,4 @@
+class AddIndustryTo < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140123234149) do
+ActiveRecord::Schema.define(version: 20140125222313) do
 
   create_table "projects", force: true do |t|
     t.string   "headline"
