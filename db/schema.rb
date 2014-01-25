9e15e86623c3de56a1cd146393e7699e17549ffc
davidhurley <dhurls99@gmail.com>
2014-01-25 17:35:13 -0500

    industry

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140125222313) do
+ActiveRecord::Schema.define(version: 20140125223341) do
 
   create_table "projects", force: true do |t|
     t.string   "headline"
